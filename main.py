9dbf4799ac7c135fd689c715b0ecc0146bb0aaa4
Thiago-jss <thiagojss11@gmail.com>
2025-08-20 11:18:45 -0300

    adicionando um valor padrão ao acess_token_expire_minutes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 app = FastAPI() 
 
