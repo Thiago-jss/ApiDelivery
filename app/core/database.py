889abe15777930d190197f9d371e5f7268b62ea1
Thiago-jss <thiagojss11@gmail.com>
2025-08-21 12:06:45 -0300

    upgrade head

--- app/core/database.py
+++ app/core/database.py
@@ -2,13 +2,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("postgresql://database_0vfg_user:WIzzUQWD1sAb5NMwZZWqY6nIUjXn78ZM@dpg-d2iu9jur433s73e9048g-a.oregon-postgres.render.com/database_0vfg", "sqlite:///./dados.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dados.DATABASE_URL")
 
 # sqlite needs special connect_args
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
 
 engine = create_engine(
-    DATABASE_URL,
+    SQLALCHEMY_DATABASE_URL,
     connect_args=connect_args,
     pool_pre_ping=True,
 )
