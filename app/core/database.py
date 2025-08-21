6c6571b9d33b75e7a5825e933da229c2d9fea504
Thiago-jss <thiagojss11@gmail.com>
2025-08-21 11:49:09 -0300

    mudando localização do  banco de dados

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dados.db")
+DATABASE_URL = os.getenv("postgresql://database_0vfg_user:WIzzUQWD1sAb5NMwZZWqY6nIUjXn78ZM@dpg-d2iu9jur433s73e9048g-a.oregon-postgres.render.com/database_0vfg", "sqlite:///./dados.db")
 
 # sqlite needs special connect_args
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
